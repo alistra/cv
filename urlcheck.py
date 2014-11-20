aa727f8bc3fa14378a1b413c1ccdbf42bd6571f1
Aleksander Balicki <balicki.aleksander@gmail.com>
2014-11-20 20:50:56 +0100

    some fixups, summary, updates

--- urlcheck.py
+++ urlcheck.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import fileinput, re, sys
-import urllib.request, urllib.error, urllib.parse
+import urllib2
 
 urls = []
 
@@ -9,10 +9,14 @@ for line in fileinput.input():
 
 for url in urls:
     try:
-        connection = urllib.request.urlopen(url)
-        if connection.getcode() != 200:
+        req = urllib2.Request(url, headers={'User-Agent' : "Magic Browser"})
+        connection = urllib2.urlopen(req)
+        if connection.getcode() > 400:
             exit(url + " FAIL http response not 200")
         connection.close()
         print(url + " OK")
-    except:
+    except urllib2.HTTPError, e:
+        print e.fp.read()
+
+    except Exception as e:
         exit(url + " FAIL exception")
